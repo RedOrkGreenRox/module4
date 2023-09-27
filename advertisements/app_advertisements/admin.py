835ff98716b574f54bfdaed3074fab10d3005a1a
Red <redcreeper001@gmail.com>
2023-09-27 20:47:44 +0300

    .

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Advertisement
 from django.utils.html import format_html
 
+
 class AdvertisementAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'description', 'img', 'price', 'created_date', 'updated_date', 'auction']
     list_filter = ['price', 'created_at', 'auction']
@@ -26,6 +27,8 @@ class AdvertisementAdmin(admin.ModelAdmin):
 
     @admin.display(description='Изображение')
     def img(self, obj):
-        return format_html('<img src="{}" style="max-width:200px; max-height:200px"/>'.format(obj.image.url)) if obj.image else None
+        return format_html(f'<img src="{obj.image.url}" style="max-width:200px; max-height:200px"/>') if obj.image else\
+            format_html('<img src="advertisement/static/img/adv.png" style='
+                        '"max-width:200px; max-height:200px"/>')
 
 admin.site.register(Advertisement, AdvertisementAdmin)
