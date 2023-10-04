deea1f08fcbf80b42315bb01f397a570a70f22ca
Red <redcreeper001@gmail.com>
2023-10-04 23:24:49 +0300

    .

--- advertisements/app_auth/urls.py
+++ advertisements/app_auth/urls.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import register_view, login_view, profile_view, logout_view
+from django.urls import path
+from .views import login_view, profile_view, logout_view, register_view
 
 urlpatterns = [
-    path('auth/', profile_view, name='auth'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path("login/", login_view, name='login'),
+    path("profile/", profile_view, name="profile"),
+    path("logout/", logout_view, name='logout'),
     path('register/', register_view, name='register'),
 ]
--- advertisements/app_auth/views.py
+++ advertisements/app_auth/views.py
@@ -1,46 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
-
-
-class UserCreationForm:
-    username = forms.CharField(label='username', min_length=5, max_length=150)
-    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
-
-    def username_clean(self):
-        username = self.cleaned_data['username'].lower()
-        new = User.objects.filter(username=username)
-        if new.count():
-            raise ValidationError("User Already Exist")
-        return
-
-    def clean_password2(self):
-        password1 = self.cleaned_data['password1']
-        password2 = self.cleaned_data['password2']
-
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Password don't match")
-        return password2
-
-    def save(self, commit=True):
-        user = User.objects.create_user(
-            self.cleaned_data['username'],
-            self.cleaned_data['password1']
-        )
-        return user
-
-
-def profile_view(request):
-    return render(request, 'app_auth/profile.html')
+from .forms import ExtendedUserCreationForm
 
 
 def login_view(request):
-    redirect_url = reverse('auth')
-    if request.method == 'GET':
+    redirect_url = reverse('profile')
+    if request.method == "GET":
         if request.user.is_authenticated:
             return redirect(redirect_url)
         else:
@@ -51,7 +18,7 @@ def login_view(request):
     if user is not None:
         login(request, user)
         return redirect(redirect_url)
-    return render(request, 'app_auth/login.html', {'error': 'Пользователь не найден.'})
+    return render(request, 'app_auth/login.html', {"error": "Пользователь не найден"})
 
 
 @login_required(login_url=reverse_lazy('login'))
@@ -65,15 +32,18 @@ def logout_view(request):
 
 
 def register_view(request):
-    if request.POST == 'POST':
-        form = UserCreationForm()
+    if request.method == "POST":
+        form = ExtendedUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user = authenticate(username=user.username, password=request.POST['password1'])
+            login(request, user=user)
+            return redirect(reverse('profile'))
     else:
-        form = UserCreationForm()
+        form = ExtendedUserCreationForm()
+
     context = {
         'form': form
     }
+
     return render(request, 'app_auth/register.html', context)
-
-
