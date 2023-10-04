b8bf67148cf4d55a2e60fbef8fc2d85bf33dbe1e
Red <redcreeper001@gmail.com>
2023-10-04 20:31:44 +0300

    .

--- advertisements/app_auth/views.py
+++ advertisements/app_auth/views.py
@@ -2,6 +2,36 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+class UserCreationForm:
+    username = forms.CharField(label='username', min_length=5, max_length=150)
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    def username_clean(self):
+        username = self.cleaned_data['username'].lower()
+        new = User.objects.filter(username=username)
+        if new.count():
+            raise ValidationError("User Already Exist")
+        return
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['password1']
+        )
+        return user
 
 
 def profile_view(request):
@@ -32,3 +62,18 @@ def profile_view(request):
 def logout_view(request):
     logout(request)
     return redirect(reverse('login'))
+
+
+def register_view(request):
+    if request.POST == 'POST':
+        form = UserCreationForm()
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'register.html', context)
+
+
